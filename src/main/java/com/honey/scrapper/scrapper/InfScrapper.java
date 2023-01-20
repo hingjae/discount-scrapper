149c24fcace4adb4c08f22f5acb3de732fb0805b
hingjae <ljh23270@naver.com>
2023-01-20 23:03:40 +0900

    인프런 scrapper

--- src/main/java/com/honey/scrapper/scrapper/InfScrapper.java
+++ src/main/java/com/honey/scrapper/scrapper/InfScrapper.java
@@ -1,5 +1,7 @@
 package com.honey.scrapper.scrapper;
 
+import com.honey.scrapper.Course;
+import com.honey.scrapper.url.Url;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -7,31 +9,49 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-public class DcScrapper {
+public class InfScrapper implements Scrapper {
 
-    final String dcUrl;
-    Connection conn;
+    private String url = Url.INFLEARN.getText();
+    private Connection conn;
 
-    public DcScrapper(String dcUrl) {
-        this.dcUrl = dcUrl;
-        this.conn = Jsoup.connect(dcUrl);
+    public InfScrapper() {
+        this.conn = Jsoup.connect(url);
     }
 
-//    final String dcUrl = "https://www.dcinside.com/";
-//    Connection conn = Jsoup.connect(dcUrl);
-
+    @Override
     public void scrap() {
         Document document = extractDocument();
-        Elements timeBest = document.select("div.time_best");
-        Elements contentList = timeBest.select("ul.p_1");
-        System.out.println("contentList = " + contentList);
-        for (Element element : contentList) {
-//            System.out.println("element = " + element.text());
+        Elements timeDiscounted = document.select("div.courses_container");
+        Elements courses = timeDiscounted.select("div.card");
+        Elements coursesAtags = courses.select("a.course_card_front");
+
+        extractInfo(coursesAtags);
+    }
+
+    private void extractInfo(Elements coursesAtags) {
+        for (Element info : coursesAtags) {
+            String url = info.select("a.course_card_front").attr("href");
+            String title = info.select("div.course_title").text();
+            String instructor = info.select("div.instructor").text();
+            String price = info.select("div.price").text();
+            String discountPercent = info.select("div.course_card_ribbon").text();
+            Course course = new Course(url, title, instructor, Integer.parseInt(price), discountPercent);
         }
     }
 
-    private Document extractDocument() {
+    @Override
+    public void extractPagination(Document document) {
+        Elements pagination = document.select("nav.pagination");
+        int pageLength = pagination.select("ul.pagination-list").select("li").size();
+        System.out.println("pagination = " + pagination);
+        System.out.println("pageLength = " + pageLength);
+    }
+
+    @Override
+    public Document extractDocument() {
         try {
             Document document = conn.get();
             return document;
@@ -39,4 +59,5 @@ public class DcScrapper {
             throw new RuntimeException(e);
         }
     }
+
 }
