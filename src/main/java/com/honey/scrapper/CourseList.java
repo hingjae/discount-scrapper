b4600c2f899f2a8dbf91f4d534c13047fccdd4c9
hingjae <ljh23270@naver.com>
2023-01-20 23:03:16 +0900

    강의 리스트

--- src/main/java/com/honey/scrapper/CourseList.java
+++ src/main/java/com/honey/scrapper/CourseList.java
@@ -1,2 +1,16 @@
-package com.honey.scrapper;public class CourseList {
+package com.honey.scrapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CourseList {
+    private List<Course> courses = new ArrayList<>();
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void addItem(Course course) {
+        courses.add(course);
+    }
 }
