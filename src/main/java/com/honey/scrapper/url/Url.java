19753ac795950bc62d4a52c41c4d62700a8710a1
hingjae <ljh23270@naver.com>
2023-01-20 23:04:27 +0900

    url 모음

--- src/main/java/com/honey/scrapper/url/Url.java
+++ src/main/java/com/honey/scrapper/url/Url.java
@@ -1,2 +1,15 @@
-package com.honey.scrapper.url;public enum Url {
+package com.honey.scrapper.url;
+
+public enum Url {
+    INFLEARN("https://www.inflearn.com/courses?discounted=true&order=seq");
+
+    private String text;
+
+    Url(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
 }
