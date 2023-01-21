d93d3293dd049eef10e0ddd6f4e27a210deeacc1
hingjae <ljh23270@naver.com>
2023-01-21 22:14:45 +0900

    homeController url 맵핑, 값 전달

--- src/main/java/com/honey/scrapper/controller/HomeController.java
+++ src/main/java/com/honey/scrapper/controller/HomeController.java
@@ -1,13 +1,22 @@
 package com.honey.scrapper.controller;
 
+import com.honey.scrapper.Course;
+import com.honey.scrapper.service.ScrapperService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
-    @GetMapping
-    public String home() {
-        return "index";
+    ScrapperService scrapperService = new ScrapperService();
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<Course> courses = scrapperService.loadPage();
+        model.addAttribute("courses", courses);
+        return "home";
     }
 }
