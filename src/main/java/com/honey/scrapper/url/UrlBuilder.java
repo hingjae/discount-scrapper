a664acd604a3e9292b005e66a4730e41fbff9c4d
hingjae <ljh23270@naver.com>
2023-01-21 22:16:14 +0900

    url 빌더

--- src/main/java/com/honey/scrapper/url/UrlBuilder.java
+++ src/main/java/com/honey/scrapper/url/UrlBuilder.java
@@ -1,2 +1,8 @@
-package com.honey.scrapper.url;public class UrlBuilder {
+package com.honey.scrapper.url;
+
+public class UrlBuilder {
+    public static String pageUp(String url, int pageNumber) {
+        url = url + "&page=" + pageNumber;
+        return url;
+    }
 }
