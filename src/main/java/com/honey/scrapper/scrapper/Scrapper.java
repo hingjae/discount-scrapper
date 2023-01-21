7fe8cf0ddc8fdc11d527eaec6cecbaeaf09e6626
hingjae <ljh23270@naver.com>
2023-01-21 22:15:58 +0900

    scrapper 인터페이스

--- src/main/java/com/honey/scrapper/scrapper/Scrapper.java
+++ src/main/java/com/honey/scrapper/scrapper/Scrapper.java
@@ -1,11 +1,18 @@
 package com.honey.scrapper.scrapper;
 
+import com.honey.scrapper.Course;
+import com.honey.scrapper.CourseList;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 public interface Scrapper {
-    void scrap();
+    void scrap(Document document);
 
-    void extractPagination(Document document);
+    int extractPagination(Document document);
 
-    Document extractDocument();
+    Document extractDocument(String url);
+
+    Course extractInfo(Element info);
+
+    CourseList getCourseList();
 }
