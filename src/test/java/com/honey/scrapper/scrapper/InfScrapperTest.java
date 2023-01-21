e530aa13544483f74b61fa0e52975965e11d0fa8
hingjae <ljh23270@naver.com>
2023-01-21 22:18:07 +0900

    scrapper 테스트

--- src/test/java/com/honey/scrapper/scrapper/InfScrapperTest.java
+++ src/test/java/com/honey/scrapper/scrapper/InfScrapperTest.java
@@ -1,18 +1,35 @@
 package com.honey.scrapper.scrapper;
 
+import com.honey.scrapper.url.Url;
+import com.honey.scrapper.url.UrlBuilder;
+import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.Test;
 
 class InfScrapperTest {
 
     InfScrapper infScrapper = new InfScrapper();
+    String url = Url.INFLEARN.getText();
+    Document document = infScrapper.extractDocument(url);
 
     @Test
     void scrap() {
-        infScrapper.scrap();
+        infScrapper.scrap(document);
     }
 
     @Test
     void extractPagination() {
-        infScrapper.extractPagination(infScrapper.extractDocument());
+        infScrapper.extractPagination(infScrapper.extractDocument(url));
     }
+
+    @Test
+    void pageUp_and_extractDocument() {
+        int page = infScrapper.extractPagination(infScrapper.extractDocument(url));
+    }
+
+    @Test
+    void pageUp() {
+        url = UrlBuilder.pageUp(url, 2);
+        System.out.println("url = " + url);
+    }
+
 }
--- src/test/java/com/honey/scrapper/url/UrlBuilderTest.java
+++ src/test/java/com/honey/scrapper/url/UrlBuilderTest.java
@@ -1,4 +1,17 @@
+package com.honey.scrapper.url;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class UrlBuilderTest {
-  
+
+    UrlBuilder urlBuilder = new UrlBuilder();
+
+    @Test
+    void 다음페이지_url_추출() {
+        String url = "https://www.inflearn.com/courses?discounted=true&order=seq";
+        String newUrl = urlBuilder.pageUp(url, 1);
+        System.out.println("newUrl = " + newUrl);
+    }
 }
