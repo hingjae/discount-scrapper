ec06b77a8c68772422a127c63630ec11e76b89db
hingjae <ljh23270@naver.com>
2023-01-22 19:38:53 +0900

    생성자 주입

--- src/main/java/com/honey/scrapper/controller/HomeController.java
+++ src/main/java/com/honey/scrapper/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.honey.scrapper.controller;
 
 import com.honey.scrapper.Course;
 import com.honey.scrapper.service.ScrapperService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +12,12 @@ import java.util.List;
 @Controller
 public class HomeController {
 
-    ScrapperService scrapperService = new ScrapperService();
+    private final ScrapperService scrapperService;
+
+    @Autowired
+    public HomeController(ScrapperService scrapperService) {
+        this.scrapperService = scrapperService;
+    }
 
     @GetMapping("/")
     public String home(Model model) {
