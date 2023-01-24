84e151c7d0eb772259591fe732d45a36d4f62616
hingjae <ljh23270@naver.com>
2023-01-24 23:28:49 +0900

    test

--- src/test/java/com/honey/scrapper/service/ScrapperServiceTest.java
+++ src/test/java/com/honey/scrapper/service/ScrapperServiceTest.java
@@ -1,5 +1,6 @@
 package com.honey.scrapper.service;
 
+import com.honey.scrapper.scrapper.InfScrapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -7,7 +8,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ScrapperServiceTest {
 
-    ScrapperService scrapperService = new ScrapperService();
+//    @Autowired
+    ScrapperService scrapperService = new ScrapperService(new InfScrapper());
 
     @Test
     void loadPage() {
