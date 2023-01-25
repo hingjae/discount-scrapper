abb9f6ee81039062c25be6cbab613d3c1fe601cc
hingjae <ljh23270@naver.com>
2023-01-25 21:38:05 +0900

    테스트

--- src/test/java/com/honey/scrapper/LombokTest.java
+++ src/test/java/com/honey/scrapper/LombokTest.java
@@ -1,2 +1,14 @@
-package com.honey.scrapper;public class LombokTest {
+package com.honey.scrapper;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LombokTest {
+
+    @Test
+    void setter() {
+        Course course = new Course();
+        course.setTitle("honey");
+        Assertions.assertThat(course.getTitle()).isEqualTo("honey");
+    }
 }
