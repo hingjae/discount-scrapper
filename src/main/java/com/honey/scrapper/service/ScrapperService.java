bdb825b353479657147490418532c31c99f5b5b2
hingjae <ljh23270@naver.com>
2023-01-29 23:04:29 +0900

    repository가 비었을 때만 scrap을 실행

--- src/main/java/com/honey/scrapper/service/ScrapperService.java
+++ src/main/java/com/honey/scrapper/service/ScrapperService.java
@@ -1,10 +1,11 @@
 package com.honey.scrapper.service;
 
-import com.honey.scrapper.AppConfig;
 import com.honey.scrapper.Course;
+import com.honey.scrapper.repository.CourseRepository;
 import com.honey.scrapper.scrapper.Scrapper;
 import com.honey.scrapper.url.Url;
 import com.honey.scrapper.url.UrlBuilder;
+import lombok.RequiredArgsConstructor;
 import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -14,25 +15,25 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ScrapperService {
 
-//    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    //    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 //    Scrapper scrapper = ac.getBean("scrapper", Scrapper.class);
     private final Scrapper scrapper;
+    private final CourseRepository courseRepository;
 
-    @Autowired
-    public ScrapperService(Scrapper scrapper) {
-        this.scrapper = scrapper;
-    }
-
-    String infUrl = Url.INFLEARN.getText();
+    private final String infUrl = Url.INFLEARN.getText();
 
     public List<Course> loadPage() {
-        List<Course> courses = getCourses();
-        return courses;
+        if (courseRepository.findAllCourses().size() == 0) {
+            List<Course> courses = getCourses();
+            return courses;
+        }
+        return courseRepository.findAllCourses();
     }
 
-    private List<Course> getCourses() {
+    public List<Course> getCourses() {
         Document document = scrapper.extractDocument(infUrl);
         int pageLength = scrapper.extractPagination(document);
         for (int i = 1; i <= pageLength; i++) {
@@ -40,7 +41,7 @@ public class ScrapperService {
             document = scrapper.extractDocument(infUrlPageup);
             scrapper.scrap(document);
         }
-        List<Course> courses = scrapper.getCourseList().getCourses();
+        List<Course> courses = courseRepository.findAllCourses();
         return courses;
     }
 }
