9f882e2fa7e5291e9f6d51e8f573eb74d3c95973
hingjae <ljh23270@naver.com>
2023-01-29 23:03:01 +0900

    Repository 추가

--- src/main/java/com/honey/scrapper/scrapper/Scrapper.java
+++ src/main/java/com/honey/scrapper/scrapper/Scrapper.java
@@ -1,7 +1,6 @@
 package com.honey.scrapper.scrapper;
 
 import com.honey.scrapper.Course;
-import com.honey.scrapper.CourseList;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
@@ -14,5 +13,4 @@ public interface Scrapper {
 
     Course extractInfo(Element info);
 
-    CourseList getCourseList();
 }
