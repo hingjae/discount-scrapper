10c377d104a5f2386c3472d0016da32e0ffd9da5
hingjae <ljh23270@naver.com>
2023-01-29 22:59:10 +0900

    Repository 인터페이스

--- src/main/java/com/honey/scrapper/repository/CourseRepository.java
+++ src/main/java/com/honey/scrapper/repository/CourseRepository.java
@@ -1,2 +1,11 @@
-package com.honey.scrapper.repository;public interface CourseRepository {
+package com.honey.scrapper.repository;
+
+import com.honey.scrapper.Course;
+
+import java.util.List;
+
+public interface CourseRepository {
+    Course save(Course course);
+
+    List<Course> findAllCourses();
 }
