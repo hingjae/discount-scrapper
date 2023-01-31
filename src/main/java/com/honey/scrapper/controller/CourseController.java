06e392616ed373c91527daff0edae94884dec6c7
hingjae <ljh23270@naver.com>
2023-01-31 20:09:48 +0900

    controller 분리

--- src/main/java/com/honey/scrapper/controller/CourseController.java
+++ src/main/java/com/honey/scrapper/controller/CourseController.java
@@ -1,2 +1,34 @@
-package com.honey.scrapper.controller;public class CourseController {
+package com.honey.scrapper.controller;
+
+import com.honey.scrapper.Course;
+import com.honey.scrapper.service.ScrapperService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class CourseController {
+
+    private final ScrapperService scrapperService;
+
+    @Autowired
+    public CourseController(ScrapperService scrapperService) {
+        this.scrapperService = scrapperService;
+    }
+
+    @GetMapping("/inflearn-courses")
+    public String inflearnCourses(Model model) {
+        List<Course> courses = scrapperService.loadPage();
+        model.addAttribute("courses", courses);
+        return "inflearnCourses";
+    }
+
+    @GetMapping("inflearn-courses-load")
+    public String inflearnCoursesLoad() {
+        scrapperService.refresh();
+        return "redirect:/inflearn-courses";
+    }
 }
--- src/main/java/com/honey/scrapper/controller/HomeController.java
+++ src/main/java/com/honey/scrapper/controller/HomeController.java
@@ -1,39 +1,13 @@
 package com.honey.scrapper.controller;
 
-import com.honey.scrapper.Course;
-import com.honey.scrapper.service.ScrapperService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @Controller
 public class HomeController {
 
-    private final ScrapperService scrapperService;
-
-    @Autowired
-    public HomeController(ScrapperService scrapperService) {
-        this.scrapperService = scrapperService;
-    }
-
     @GetMapping("/")
     public String home() {
         return "index";
-    }
-
-    @GetMapping("/inflearn-courses")
-    public String inflearnCourses(Model model) {
-        List<Course> courses = scrapperService.loadPage();
-        model.addAttribute("courses", courses);
-        return "inflearnCourses";
-    }
-
-    @GetMapping("inflearn-courses-load")
-    public String inflearnCoursesLoad() {
-        scrapperService.refresh();
-        return "redirect:/inflearn-courses";
     }
 }
